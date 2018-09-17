dc975fb28dc000b67ef3a7f48847580ddaa60986
Deeter <deeter.cesler@gmail.com>
2018-09-17 17:47:11 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 console.log("HOMEWORK BAY-BEEE");
+
+// 1.
+// boolean, a variable that contains it
+// string, perhaps as the value in a key/value pair in an object
+// an object, with each feature (e.g. hull, lasers) being a key
+// an array of strings, with the names as strings
+// an array of arrays. each child array has two values, name (string) and location (string)
+// an object for "the class" with each student being a key, and each student key containing 
+    // keys fora name, location, and a list. each list is an array of strings of 
+    // their favorite TV shows
+
+// 2.
+const rainbow = ["red", "orange", "yellow", "green", "blue", "violet"];
+rainbow[4];
+const deeter = {
+    food: "burger",
+    hobby: "skiing",
+    town: "Denver",
+    datatype: "objects"
+};
+deeter.hobby;
